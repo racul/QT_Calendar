53f05f91054970c7d367cef0a73ab07579e5dd5f
racul <49424339+racul@users.noreply.github.com>
2024-10-10 17:18:27 +0900

    Delete build directory

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calender_FILE C:/Users/JeongMin/Documents/Calender/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/Calender.exe)
-set(__QT_DEPLOY_TARGET_Calender_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calender_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
